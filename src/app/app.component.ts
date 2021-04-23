30d5c434f4de16ac6eee71294236e10d65e7819f
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-23 16:19:35 -0400

    seccion 10 clase 131

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipeApp';
+  nombre : string = 'pablo alvarez';
+  valor : number = 1000;
+  obj ={
+    nombre: 'fernando',
+    edad : 50
+  }
+
+  mostrarNombre(){
+      console.log(this.nombre);
+      console.log(this.valor);
+      console.log(this.obj);
+
+  }
+
+
+
 }
