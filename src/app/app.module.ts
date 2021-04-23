a4f4d0947900c6d96057bda1af5b2b5243cce9b1
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-23 18:05:22 -0400

    seccion 10 clase 134

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 
 //primeNg
 import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
 
 
 @NgModule({
@@ -13,7 +15,8 @@ import {ButtonModule} from 'primeng/button';
   ],
   imports: [
     BrowserModule,
-    ButtonModule
+    ButtonModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
