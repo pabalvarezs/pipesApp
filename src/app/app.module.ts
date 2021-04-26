9a1371bfaf590c8d1f535c26a648bbafeac6dff5
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-26 12:14:12 -0400

    seccion 10 clase 142

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,7 +8,11 @@ import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
 
 
+//Cambiar el locale de la app
 
+import  localeEs from "@angular/common/locales/es-CL";
+import { registerLocaleData } from "@angular/common";
+registerLocaleData(localeEs);
 
 // //primeNg
 // import {ButtonModule} from 'primeng/button';
@@ -27,7 +31,9 @@ import { VentasModule } from './ventas/ventas.module';
     VentasModule
 
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue : 'es-CL' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
