b44983f5e83b89d53189cfba1a172562d870a00f
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-26 12:26:28 -0400

    seccion 10 clase 143

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,11 @@ import { VentasModule } from './ventas/ventas.module';
 //Cambiar el locale de la app
 
 import  localeEs from "@angular/common/locales/es-CL";
+import  localeFr from "@angular/common/locales/fr";
+
 import { registerLocaleData } from "@angular/common";
 registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 // //primeNg
 // import {ButtonModule} from 'primeng/button';
