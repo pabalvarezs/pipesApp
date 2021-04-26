6ff97847d42f87cad229c59cf6b5a28217a44c7c
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-26 10:45:13 -0400

    seccion 10 clase 138

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -10,38 +10,49 @@ import {MenuItem} from 'primeng/api';
   styles: [
   ]
 })
+
 export class MenuComponent implements OnInit {
 
   items: MenuItem[] = [];
   
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
 
-    this.items = [
+    this.items = 
+    [
+    
       {
-          label: 'File',
-          items: [{
-                  label: 'New', 
-                  icon: 'pi pi-fw pi-plus',
-                  items: [
-                      {label: 'Project'},
-                      {label: 'Other'},
-                  ]
-              },
-              {label: 'Open'},
-              {label: 'Quit'}
-          ]
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items:
+        [
+          {
+            label: 'Textos y fechas',
+            icon: 'pi pi-align-left',
+            routerLink: '/'
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+            routerLink: 'numeros'
+          },
+          {
+            label: 'No Comunes',
+            icon: 'pi pi-globe',
+            routerLink: 'no-comunes'
+          },
+        ]
+
       },
       {
-          label: 'Edit',
-          icon: 'pi pi-fw pi-pencil',
-          items: [
-              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-          ]
+        label: 'Pipes Personalizados',
+        icon:'pi pi-cog'
+        
       }
-  ];
+    
+    
+    ];
   }
-
 }
