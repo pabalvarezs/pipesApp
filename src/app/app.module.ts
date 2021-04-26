667942064b8b2a46db043e078b8682f5331edb6d
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-26 11:03:59 -0400

    seccion 10 clase 139

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
 
 
 
@@ -22,7 +23,8 @@ import { AppRouterModule } from './app-router.module';
   imports: [
     AppRouterModule,
     BrowserModule,
-    SharedModule
+    SharedModule,
+    VentasModule
 
   ],
   providers: [],
