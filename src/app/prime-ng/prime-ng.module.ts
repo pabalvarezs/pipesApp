74f3841e17eba324c7c7a377f7e461a27daef4fa
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-27 12:18:16 -0400

    seccion 10 final clase 160

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -6,9 +6,8 @@ import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
 import {FieldsetModule} from 'primeng/fieldset';
 import {MenubarModule} from 'primeng/menubar';
-
-
-
+import {TableModule} from 'primeng/table';
+import {ToolbarModule} from 'primeng/toolbar';
 
 
 @NgModule({
@@ -16,7 +15,10 @@ import {MenubarModule} from 'primeng/menubar';
     ButtonModule,
     CardModule,
     FieldsetModule,
-    MenubarModule
+    MenubarModule,
+    TableModule,
+    ToolbarModule
   ]
 })
+
 export class PrimeNgModule { }
